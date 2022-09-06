5193f41ddabd8aa4077f40f1602fed43049511a7
suman.lucifer94@gmail.com <suman.lucifer94@gmail.com>
2022-09-06 04:42:18 +0000

    test sept 6

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -735,16 +735,20 @@ sap.ui.define([
 
 		onBeforeUploadStarts: function (oEvent) {
 			var _self = this;
+			var lcv = 0;
 			var fileContent = "";
 			var oCustomerHeaderSlug = new sap.m.UploadCollectionParameter({
 				name: "slug",
 				// value: oEvent.getParameter("fileName") + "|" + _self._oMessageId
-				value: this.fileContent + "|" + this.vbeln + "|" + this.fileName + "|" + this.fileType + "|" + this.fileSize
+				value: this.fileContent + "|" + this.vbeln + "|" + oEvent.getSource()._aFilesFromDragAndDropForPendingUpload[lcv].name + "|" +
+					oEvent.getSource()._aFilesFromDragAndDropForPendingUpload[lcv].type + "|" + oEvent.getSource()._aFilesFromDragAndDropForPendingUpload[
+						lcv].size
 					// value: "" + "|" + this.vbeln + "|" + this.fileName + "|" + this.fileType + "|" + this.fileSize
 			});
 
 			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
 			setTimeout(function () {}, 40000);
+			lcv = lcv + 1;
 		},
 		onUploadComplete: function (oEvent) {
 			var _that = this;
@@ -761,7 +765,9 @@ sap.ui.define([
 						this._responseReceivedCnt = 0 + this._responseReceivedCnt + _responseReceivedlen;
 						if (oEvent.getParameter("files")[j].status === 201) {
 							oUploadCollection.removeItem(this._allItems[i]);
-							_that.getView().byId("idAttachmentsTable").getModel("AttachmentsModel").refresh();
+							// 	_that.getView().byId("idAttachmentsTable").getModel("AttachmentsModel").refresh();
+							_that.getAttachments();
+							_that._getAddAttachments().close();
 						} else {
 							// 			var responceFile = oEvent.getParameter("files")[j].reponse;
 							// 			var slpliteString = responceFile.split("/");
@@ -782,7 +788,99 @@ sap.ui.define([
 			}
 
 		},
-		// End of File upload event handelers //	
+		// End of File upload event handelers //
+
+		// 		onChange: function (oEvent) {
+		// 			var oUploadCollection = oEvent.getSource();
+		// 			var oModel = this.getOwnerComponent().getModel();
+		// 			var sectoken = oModel.getSecurityToken();
+
+		// 			// Header Token
+		// 			var oCustomerHeaderToken = new sap.m.UploadCollectionParameter({
+		// 				name: "x-csrf-token",
+		// 				value: sectoken
+		// 			});
+
+		// 					this.fileContent = null;
+		// 					this.vbeln = this.getView().byId("idSaleOrderInput").getValue();
+		// 					this.fileName = oEvent.getParameters().files[0].name;
+		// 					this.fileType = oEvent.getParameters().files[0].type;
+		// 					this.fileSize = oEvent.getParameters().files[0].size;
+
+		// 			oUploadCollection.addHeaderParameter(oCustomerHeaderToken);
+		// 			// 			MessageToast.show("Event change triggered");
+		// 		},
+
+		// 		onFileDeleted: function (oEvent) {
+		// 			// 			MessageToast.show("Event fileDeleted triggered");
+		// 		},
+
+		// 		onFilenameLengthExceed: function (oEvent) {
+		// 			// 			MessageToast.show("Event filenameLengthExceed triggered");
+		// 		},
+
+		// 		onFileSizeExceed: function (oEvent) {
+		// 			// 			MessageToast.show("Event fileSizeExceed triggered");
+		// 		},
+
+		// 		onTypeMissmatch: function (oEvent) {
+		// 			// 			MessageToast.show("Event typeMissmatch triggered");
+		// 		},
+
+		// 		onStartUpload: function (oEvent) {
+		// 			var oUploadCollection = sap.ui.getCore().byId("idFileUploadCollection");
+		// 			// 			var oTextArea = this.byId("TextArea");
+		// 			var cFiles = oUploadCollection.getItems().length;
+		// 			var uploadInfo = cFiles + " file(s)";
+
+		// 			if (cFiles > 0) {
+		// 				oUploadCollection.upload();
+
+		// 				// if (oTextArea.getValue().length === 0) {
+		// 				// 	uploadInfo = uploadInfo + " without notes";
+		// 				// } else {
+		// 				// 	uploadInfo = uploadInfo + " with notes";
+		// 				// }
+
+		// 				// MessageToast.show("Method Upload is called (" + uploadInfo + ")");
+		// 				// MessageBox.information("Uploaded " + uploadInfo);
+		// 				// oTextArea.setValue("");
+		// 			}
+		// 		},
+
+		// 		onBeforeUploadStarts: function (oEvent) {
+		// 			// Header Slug
+		// 			var oCustomerHeaderSlug = new UploadCollectionParameter({
+		// 				name: "slug",
+		// 				value: oEvent.getParameter("fileName")
+		// 			});
+		// 			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
+		// 			setTimeout(function () {
+		// 				MessageToast.show("Event beforeUploadStarts triggered");
+		// 			}, 4000);
+		// 		},
+
+		// 		onUploadComplete: function (oEvent) {
+		// 			var sUploadedFileName = oEvent.getParameter("files")[0].fileName;
+		// 			setTimeout(function () {
+		// 				var oUploadCollection = this.byId("UploadCollection");
+
+		// 				for (var i = 0; i < oUploadCollection.getItems().length; i++) {
+		// 					if (oUploadCollection.getItems()[i].getFileName() === sUploadedFileName) {
+		// 						oUploadCollection.removeItem(oUploadCollection.getItems()[i]);
+		// 						break;
+		// 					}
+		// 				}
+
+		// 				// delay the success message in order to see other messages before
+		// 				MessageToast.show("Event uploadComplete triggered");
+		// 			}.bind(this), 8000);
+		// 		},
+
+		// 		onSelectChange: function (oEvent) {
+		// 			var oUploadCollection = this.byId("UploadCollection");
+		// 			oUploadCollection.setShowSeparators(oEvent.getParameters().selectedItem.getProperty("key"));
+		// 		}
 
 	});
 });
